43966569c5c7263eb86b851cdacd2f26720a230b
John McCabe <john@mccabe.org.uk>
2016-11-16 15:14:51 +0000

    Initial commit to github.

---
+++ mod_jmnewsticker.php
@@ -0,0 +1,29 @@
+<?php
+/**
+* @module	JM News Ticker
+* @copyright	Copyright (C) 2016 John McCabe
+* @license	GPL
+*/
+
+defined('_JEXEC') or die('Restricted access');
+error_reporting(E_ERROR);
+define ("DS", DIRECTORY_SEPARATOR);
+
+require_once __DIR__ . '/helper.php';
+
+/* Handle the JQuery and Javascript stuff */
+$document = &JFactory::getDocument();
+
+$loadJ = $params->get('loadJ', true);
+if ($loadJ) {
+  $document->addScript( JURI::root() . 'modules/mod_jmnewsticker/js/jquery.js' );
+}
+$document->addScript( JURI::root() . 'modules/mod_jmnewsticker/js/jquery.nc.js' );
+$document->addScript( JURI::root() . 'modules/mod_jmnewsticker/js/jquery.ticker.js' );
+$document->addStylesheet( JURI::root() . 'modules/mod_jmnewsticker/css/style.css' );
+
+/* Get the data we're going to show from the helper */
+$rows = mod_jmnewstickerHelper::getList($params);
+
+/* Need to pull in the selected layout file */
+require JModuleHelper::getLayoutPath('mod_jmnewsticker', $params->get('layout', 'default'));
